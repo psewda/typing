0ec852d4fee5683ecc85c196add8587d3e78c7b1
Prakash Sewda <psewdablr@gmail.com>
2021-04-08 06:18:23 +0000

    chore: version bump - v0.2.0

--- version.go
+++ version.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Version is the place holder to put version string.
-var Version = "0.1.0"
+var Version = "0.2.0"
 
 // BuildNumber is the place holder to put build-number
 // string. It is set dynamically in the ci/cd pipeline.
